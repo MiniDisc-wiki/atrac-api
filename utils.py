5c25572704adaa6a0ced3d38ea3ec8ad83e6beab
Logan Brown <thinkbrown@96west.net>
2023-02-11 12:54:57 -0500

    Add ATRAC3+ support

--- utils.py
+++ utils.py
@@ -6,12 +6,30 @@ from uuid import uuid4
 
 
 class atracTypes(str, Enum):
-  LP2 = 'LP2'
-  LP4 = 'LP4'
+  LP2     = 'LP2'
+  LP4     = 'LP4'
+  PLUS48  = 'PLUS48'
+  PLUS64  = 'PLUS64'
+  PLUS96  = 'PLUS96'
+  PLUS128 = 'PLUS128'
+  PLUS160 = 'PLUS160'
+  PLUS192 = 'PLUS192'
+  PLUS256 = 'PLUS256'
+  PLUS320 = 'PLUS320'
+  PLUS352 = 'PLUS352'
 
 bitrates = {
-  'LP2': 132,
-  'LP4': 66
+  'LP2':     132,
+  'LP4':     66,
+  'PLUS48':  48,
+  'PLUS64':  64,
+  'PLUS96':  96,
+  'PLUS128': 128,
+  'PLUS160': 160,
+  'PLUS192': 192,
+  'PLUS256': 256,
+  'PLUS320': 320,
+  'PLUS352': 352
 }
 
 def allowed_file(filename):
